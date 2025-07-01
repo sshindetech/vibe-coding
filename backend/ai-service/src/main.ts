31200f850227c9ae09eb096acf773c89fa8ebdd5
Suryakand Shinde <suryakand.shinde@cognizant.com>
2025-07-01 09:24:30 -0400

    updated port configuration to read from env

--- backend/ai-service/src/main.ts
+++ backend/ai-service/src/main.ts
@@ -5,7 +5,7 @@ import { AiServiceModule } from './ai-service.module';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AiServiceModule, {
     transport: Transport.TCP,
-    options: { port: 4002 },
+    options: { port: process.env.AI_SERVICE_PORT ? Number(process.env.AI_SERVICE_PORT) : 4002 },
   });
   await app.listen();
 }
--- backend/excel-service/src/main.ts
+++ backend/excel-service/src/main.ts
@@ -5,7 +5,7 @@ import { ExcelServiceModule } from './excel-service.module';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(ExcelServiceModule, {
     transport: Transport.TCP,
-    options: { port: 4001 },
+    options: { port: process.env.EXCEL_SERVICE_PORT ? Number(process.env.EXCEL_SERVICE_PORT) : 4001 },
   });
   await app.listen();
 }
